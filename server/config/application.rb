5d5a2e5fd3cbf1f0bcaf5911367d941cbb577b33
Shubhashish Verma <svashish305@gmail.com>
2023-03-24 21:31:40 +0530

    updated config env

--- server/config/application.rb
+++ server/config/application.rb
@@ -23,7 +23,5 @@ module Server
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
-
-    config.secret_key_base = '<%= ENV["SECRET_KEY_BASE"] %>'
   end
 end
