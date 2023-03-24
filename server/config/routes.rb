c71845a820a938fa117010eaf0e235b6e1edf620
Shubhashish Verma <svashish305@gmail.com>
2023-03-24 15:42:48 +0530

    added endpoints

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -3,5 +3,7 @@ Rails.application.routes.draw do
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
-  # root "articles#index"
+  root "questions#index"
+
+  post "/questions", to: "questions#create"
 end
