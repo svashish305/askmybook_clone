091b07e86053bf08454d552bd6d2666935aade06
Shubhashish Verma <svashish305@gmail.com>
2023-03-24 19:05:30 +0530

    added form, and main home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -3,7 +3,83 @@ import './Home.css'
 import book from '../assets/book.png'
 
 const Home = () => {
+  const formRef = useRef();
+  const [form, setForm] = useState({
+    question: "",
+  });
+  const [question, setQuestion] = useState('')
+  const [answer, setAnswer] = useState('')
+  const [audioSrcUrl, setAudioSrcUrl] = useState('')
 
+  const fetchDefaultQuestion = async () => {
+    const response = await fetch(`${import.meta.env.VITE_APP_API_URL}/questions`)
+    const data = await response.json()
+    setQuestion(data.default_question)
+  }
+
+  const handleChange = (e) => {
+    const { target } = e
+    const { name, value } = target
+
+    setForm({
+      ...form,
+      [name]: value,
+    })
+    if (name === 'question') {
+      setQuestion(value)
+    }
+  }
+
+  const fetchAnswer = () => {
+    try {
+      fetch(`${import.meta.env.VITE_APP_API_URL}/questions`, {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({ question })
+      })
+      .then(resp => resp.json())
+      .then(data => {
+        setAnswer(data.answer)
+        setAudioSrcUrl(data.audio_src_url)
+      })
+      .catch(err => console.log(err))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    if (question === "") {
+      alert("Please ask a question!")
+      return false
+    }
+    fetchAnswer()
+  }
+
+  const handleRandomQuestion = () => {
+    const options = [
+      "How long it takes to write TME?", 
+      "What is your definition of community?", 
+      "How do I decide what kind of business I should start?"
+    ];
+    const random = ~~(Math.random() * options.length);
+    setQuestion(options[random])
+  }
+
+  const handleAnotherQuestion = () => {
+    setAnswer('')
+    setQuestion('')
+    setAudioSrcUrl('')
+    formRef.current.reset()
+  }
+
+  useEffect(() => {
+    fetchDefaultQuestion()
+  }, [])
 
   return (
     <>
@@ -16,7 +92,41 @@ const Home = () => {
         </div>
       </div>
 
-      
+      <div className="main">
+        <p className="credits">This is an experiment in using AI to make my book's content more accessible. Ask a question and AI'll answer it in real-time:</p>
+
+        <form ref={formRef} onSubmit={handleSubmit}>
+          <textarea 
+            name="question" 
+            id="question" 
+            value={question}
+            onChange={handleChange}
+          />
+
+          <div className="buttons" style={{display: answer && "none"}}>
+            <button type="submit" id="ask-button">Ask question</button>
+            <button id="lucky-button" style={{background: "#eee", borderColor: "#eee", color: "#444"}} onClick={handleRandomQuestion}>I'm feeling lucky</button>
+          </div>
+        </form>
+
+        {answer ? 
+          <>
+            <p id="answer-container" className="hidden showing"><strong>Answer:</strong><span id="answer">{answer}</span>
+              <button id="ask-another-button" style={{display: "block"}} onClick={handleAnotherQuestion}>Ask another question</button>
+            </p>
+          </>
+          : 
+          <>
+            <p id="answer-container" className="hidden"><strong>Answer:</strong> <span id="answer"></span> 
+              <button id="ask-another-button" style={{display: "none"}} onClick={handleAnotherQuestion}>Ask another question</button>
+            </p>
+          </>
+        }
+
+        <audio id="audio" controls autoPlay>
+          <source src={audioSrcUrl} type="audio/wav" />
+        </audio>
+      </div>
     </>
   )
 }
