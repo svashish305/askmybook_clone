449bf1c4ba1ad2d061d5f7d46696b9582f27dc18
Shubhashish Verma <svashish305@gmail.com>
2023-03-24 19:03:22 +0530

    added header in home component

--- client/src/components/Home.jsx
+++ client/src/components/Home.jsx
@@ -1,8 +1,23 @@
-import React from 'react'
+import React, { useRef, useState, useEffect } from 'react'
+import './Home.css'
+import book from '../assets/book.png'
 
 const Home = () => {
+
+
   return (
-    <div>Home</div>
+    <>
+      <div className="header">
+        <div className="logo">
+          <a href="https://www.amazon.com/Minimalist-Entrepreneur-Great-Founders-More/dp/0593192397">
+            <img src={book} loading="lazy" />
+          </a>
+          <h1>Ask My Book</h1>
+        </div>
+      </div>
+
+      
+    </>
   )
 }
 
