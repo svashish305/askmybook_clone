85b90435e6a534e332e92fe92805253dcbba7f20
Shubhashish Verma <svashish305@gmail.com>
2023-03-24 20:54:06 +0530

    updated cors origin

--- server/config/initializers/cors.rb
+++ server/config/initializers/cors.rb
@@ -21,4 +21,12 @@ Rails.application.config.middleware.insert_before 0, Rack::Cors do
       headers: :any,
       methods: [:get, :post, :put, :patch, :delete, :options, :head]
   end
+
+  allow do
+    origins "https://beautiful-pasca-f2e46b.netlify.app/"
+
+    resource "*",
+      headers: :any,
+      methods: [:get, :post, :put, :patch, :delete, :options, :head]
+  end
 end
